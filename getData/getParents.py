b433a63537a1ca8a2d61c1dd4a35150fe9f8a3e1
2190468 <2190468@ecccomp.com>
2021-06-08 15:27:19 +0900

    2021/06/08

--- getData/getParents.py
+++ getData/getParents.py
@@ -9,21 +9,22 @@ import numpy as np
 
 url_first = []
 url_third = []
-parents = []
-get_href = []
 umamei = []
 td = []
-# 調べる年を下の配列に記入
-year_list = [2020]
-#処理が長いのでカウンターで残り時間を表示
-counter = 150
+# 結果で必要な要素
+parents = []
+get_href = []
 
-#年ごとのURLを作成
+year_list = [2020]  # 調べる年を配列に記入
+
+counter = 138  # 処理が長いのでカウンターで残り時間を表示
+
+# 年ごとのURLを作成
 for year in year_list:
     url_first.append(
         "https://www.keibalab.jp/db/race/grade.html?year="+str(year))
 
-#年ごとの重賞レースのリストWebページを表示
+# 年ごとの重賞レースのリストWebページを表示
 for url_second in url_first:
     # headersがないと404で拒否されるので記載
     # headersは下のサイトで確認可
@@ -36,29 +37,34 @@ for url_second in url_first:
 
     for race_url in soup.find_all("td", class_="bold"):
         get_href = race_url.contents[2].get("href")
-        #レース結果の詳細のURL作成→url_thirdに格納
+        # レース結果の詳細のURL作成→url_thirdに格納
         url_third_str = "https://www.keibalab.jp"+str(get_href)
         url_third.append(url_third_str)
-        soup = BeautifulSoup(requests.get(url_third_str,headers=headers).content,"lxml")
+        soup = BeautifulSoup(requests.get(
+            url_third_str, headers=headers).content, "lxml")
 
-        #馬1頭の詳細のURLを取得
-        for url in soup.find_all("table",class_="DbTable stripe resulttable"):
+        # 馬1頭の詳細のURLを取得
+        for url in soup.find_all("table", class_="DbTable stripe resulttable"):
             for tbody in url.find_all("tbody"):
                 for tr in tbody.find_all("tr"):
                     href = tr.contents[7].contents[0].get("href")
                     url_horse = "https://www.keibalab.jp" + str(href)
-                    #仔馬の名前を格納
+                    # 仔馬の名前を格納
+                    # 同じ行数でないといけないため2つ仔馬の名前を追加
                     umamei.append(tr.contents[7].contents[0].text)
-                    umamei.append(" ")
+                    umamei.append(tr.contents[7].contents[0].text)
                     # URLを読み込み
                     soupsan = BeautifulSoup(requests.get(
-                    url_horse, headers=headers).content, "lxml")
+                        url_horse, headers=headers).content, "lxml")
                     # 親馬の名前を取得
                     for parent in soupsan.find_all("span", class_='bold std15'):
                         parents.append(parent.text)
+            counter -= 1
+            print("残り"+str(counter))
+            len(umamei)
+            len(parents)
     # print(url_third)
 
 horse_parents = pd.DataFrame(
-    {"馬名":umamei,"馬親":parents})
+    {"馬名": umamei, "馬親": parents})
 race_detail.to_excel('./馬親.xlsx', header=False, index=False)
-    
