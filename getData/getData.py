7ee10fc9fa0e3e701597e5f63d3ddf38d7141412
2190468 <2190468@ecccomp.com>
2021-06-15 09:51:57 +0900

    2021/06/15

--- getData/getData.py
+++ getData/getData.py
@@ -1,3 +1,4 @@
+# coding: UTF-8
 from bs4 import BeautifulSoup
 import os
 import re
@@ -24,19 +25,27 @@ corner_array = []
 grade_array = []
 course_dis_array = []
 course_detail_array = []
+course_dis_array_sub = []
 weather_array = []
 tyakujun_array = []
+url_array = []
+status_array = []
+hour_array = []
+hour_array_sub = []
+waku_array = []
+ftime_array = []
 
-#取り出す年を配列に記入
-#~2017,2018,2019~でクラス名が分けられている
-year_list = [2018,2019,2020,2021]
+
+# 取り出す年を配列に記入
+# ~2017,2018,2019~でクラス名が分けられている
+year_list = [2019, 2020]
 
 whole_race_url_array = []
 race_url_array = []
 
-#取り出し処理がどのくらい終わっているかわかりやすく表示するためのcounter
-#別に処理に必要ではない
-#2019～2021は340レースなので
+# 取り出し処理がどのくらい終わっているかわかりやすく表示するためのcounter
+# 別に処理に必要ではない
+# 2019～2021は340レースなので
 counter = 452
 
 # yearがfor文の中に入っていないので判定で使えるようにcounterを用意
@@ -46,28 +55,30 @@ year_counter = 2018
 for year in year_list:
     whole_race_url_array.append(
         "https://www.jra.go.jp/datafile/seiseki/replay/" + str(year) + "/jyusyo.html")
-        
+
 
 # 年の分、レースのURL文回す
 for race_number in whole_race_url_array:
     race_html = requests.get(race_number)
     soup = BeautifulSoup(race_html.content, "lxml")
 
-    #結果を表示するurlのクラスが2016年から違うので判定
+    # 結果を表示するurlのクラスが2016年から違うので判定
     if year_counter >= 2016:
 
         for race_url in soup.find_all("td", class_="result"):  # 年ごとの１レースのurl
             if(race_url is not None and len(race_url) > 0):     # class="result"があるかどうかを判定
 
                 # class="result"の1つ目の子要素(a href="")のhrefのURLをrace_urlに格納
-                race_url = "https://www.jra.go.jp" + race_url.contents[0].get("href")
+                race_url = "https://www.jra.go.jp" + \
+                    race_url.contents[0].get("href")
+                race_url_str = str(race_url)
                 detail_html = requests.get(race_url)
                 soup = BeautifulSoup(detail_html.content, "lxml")
 
-                #2018年だけクラスがオリジナルなので判定
+                # 2018年だけクラスがオリジナルなので判定
                 if race_number == "https://www.jra.go.jp/datafile/seiseki/replay/2018/jyusyo.html":
-                
-                    #レースのリスト作成時必要データ
+
+                    # レースのリスト作成時必要データ
                     date_array.append(
                         soup.find("div", class_="mt10").get_text())
                     race_name_array.append(
@@ -81,16 +92,16 @@ for race_number in whole_race_url_array:
                     grade_array.append(
                         soup.find("img", class_="gradeIconImageTtl").get("alt"))
 
-                    #レースの詳細作成時必要データ
-                    #着順
+                    # レースの詳細作成時必要データ
+                    # 着順
                     for tyaku in soup.find_all("td", class_='chakuCol'):
                         tyakujun_array.append(tyaku.text)
 
-                    #馬番
+                    # 馬番
                     for umaban in soup.find_all("td", class_='umabanCol'):
                         umaban_array.append(umaban.text)
 
-                    #馬名
+                    # 馬名
                     for name in soup.find_all("td", class_='umameiCol'):
                         name_array.append(name.text)
 
@@ -102,7 +113,7 @@ for race_number in whole_race_url_array:
                     for add_weight in soup.find_all("td", class_='hutanCol'):
                         add_weight_array.append(add_weight.text)
 
-                    #騎手
+                    # 騎手
                     for jockey in soup.find_all("td", class_='jocCol'):
                         jockey_array.append(jockey.text)
 
@@ -118,7 +129,7 @@ for race_number in whole_race_url_array:
                     for margin in soup.find_all("td", class_='chakusaCol'):
                         margin_array.append(margin.text)
 
-                    #調教師
+                    # 調教師
                     for trainer in soup.find_all("td", class_='choukyoCol'):
                         trainer_array.append(trainer.text)
 
@@ -126,31 +137,86 @@ for race_number in whole_race_url_array:
                     # for corner in soup.find_all("td", class_='corner'):
                     #     corner_array.append(corner.text)
 
-                    #残りレースの処理数を表示
+                    # 残りレースの処理数を表示
                     counter -= 1
                     print(counter)
-
-
                 else:
 
-                    #レースのリスト作成時必要データ
-                    date_array.append(soup.find("div", class_="date").get_text())
-                    race_name_array.append(soup.find("span", class_="race_name").contents[0])
-                    weather_array.append(soup.find("li", class_="weather").get_text())
-                    course_dis_array.append(soup.find("div", class_="course").contents[1])
-                    course_detail_array.append(soup.find("div", class_='course').contents[3].text)
-                    grade_array.append(soup.find("span", class_="grade_icon").contents[0].get("alt"))
-                    
-                    #レースの詳細作成時必要データ
-                    #着順
+                    # レースのリスト作成時必要データ
+                    # date_array.append(soup.find(
+                    #     "div", class_="date").get_text())
+                    # race_name_array.append(
+                    #     soup.find("span", class_="race_name").contents[0])
+                    # weather_array.append(
+                    #     soup.find("li", class_="weather").get_text())
+                    # course_dis_array.append(
+                    #     soup.find("div", class_="course").contents[1])
+                    # course_detail_array.append(
+                    #     soup.find("div", class_='course').contents[3])
+                    # grade_array.append(
+                    #     soup.find("span", class_="grade_icon").contents[0].get("alt"))
+
+                    # 着順
+                    counter_num = 0
+
                     for tyaku in soup.find_all("td", class_='place'):
                         tyakujun_array.append(tyaku.text)
+                        counter_num += 1
+                    
+                    for count in range(counter_num):
+                        url_array.append(race_url_str)
 
-                    #馬番
+                    for date in soup.find_all("div", class_="date"):
+                        for count in range(counter_num):
+                            date_array.append(date.text)
+                            print(date.text)
+
+                    for race_name in soup.find_all("span", class_="race_name"):
+                        for count in range(counter_num):
+                            race_name_array.append(race_name.text)
+
+                    for weather in soup.find_all("li", class_="weather"):
+                        for count in range(counter_num):
+                            weather_array.append(weather.text)
+
+                    for course_dis in soup.find_all("div", class_="course"):
+                        for count in range(counter_num):
+                            course_dis_array.append(course_dis.contents[1])
+                    
+                    for course_dis in soup.find_all("div", class_="course"):
+                        for count in range(counter_num):
+                            course_dis_array_sub.append(course_dis.text)
+
+                    for course_detail in soup.find_all("div", class_='course'):
+                        for count in range(counter_num):
+                            course_detail_array.append(
+                                course_detail.contents[3].text)
+
+                    for grade in soup.find_all("span", class_="grade_icon"):
+                        for count in range(counter_num):
+                            grade_array.append(grade.contents[0].get("alt"))
+
+                    for status in soup.find_all("span", class_="txt")[191]:
+                        for count in range(counter_num):
+                            status_array.append(status)
+
+                    for hour in soup.find_all("strong")[0]:
+                        for count in range(counter_num):
+                            hour_array.append(hour)
+                    
+                    for hour_sub in soup.find_all("strong")[1]:
+                        for count in range(counter_num):
+                            hour_array_sub.append(hour_sub)
+                    # レースの詳細作成時必要データ
+
+                    # 枠
+                    for waku in soup.find_all("td", class_="waku"):
+                        waku_array.append(waku.contents[0].get("alt"))
+                    # 馬番
                     for umaban in soup.find_all("td", class_='num'):
                         umaban_array.append(umaban.text)
-                    
-                    #馬名
+
+                    # 馬名
                     for name in soup.find_all("td", class_='horse'):
                         name_array.append(name.text)
 
@@ -162,9 +228,13 @@ for race_number in whole_race_url_array:
                     for add_weight in soup.find_all("td", class_='weight'):
                         add_weight_array.append(add_weight.text)
 
-                    #騎手
+                    # 騎手
                     for jockey in soup.find_all("td", class_='jockey'):
                         jockey_array.append(jockey.text)
+
+                    # 推定上り
+                    for f in soup.find_all("td", class_="f_time"):
+                        ftime_array.append(f.text)
 
                     # 馬体重
                     for weight in soup.find_all("td", class_='h_weight'):
@@ -178,29 +248,28 @@ for race_number in whole_race_url_array:
                     for margin in soup.find_all("td", class_='margin'):
                         margin_array.append(margin.text)
 
-                    #調教師
+                    # 調教師
                     for trainer in soup.find_all("td", class_='trainer'):
                         trainer_array.append(trainer.text)
 
                     # コーナー通過順位
                     for corner in soup.find_all("td", class_='corner'):
                         corner_array.append(corner.text)
-                    
-                    #残りレースの処理数を表示
+
+                    # 残りレースの処理数を表示
                     counter -= 1
                     print(counter)
-                
-            
 
-            
-#レースのリストの行数
+
+# レースのリストの行数
+
+# レースの詳細の行数
 print(len(date_array))
-print(len(grade_array))
-print(len(course_dis_array))
-print(len(course_detail_array))
 print(len(weather_array))
-
-#レースの詳細の行数
+print(len(race_name_array))
+print(len(course_detail_array))
+print(len(course_dis_array))
+print(len(grade_array))
 print(len(umaban_array))
 print(len(name_array))
 print(len(age_array))
@@ -213,17 +282,19 @@ print(len(margin_array))
 print(len(corner_array))
 
 ########################################
-#エクセルファイルにするには同じ行数でないといけない
-#なので二つのファイルに分けた
-#もしレースの詳細のデータが見にくかったら配列に空白を入れてレースごとに分ける
+# エクセルファイルにするには同じ行数でないといけない
+# なので二つのファイルに分けた
+# もしレースの詳細のデータが見にくかったら配列に空白を入れてレースごとに分ける
 #########################################
 
-#レースのリストをエクセルファイルにエクスポート
+# レースのリストをエクセルファイルにエクスポート
 # race_list = pd.DataFrame({'日付': date_array, 'レース名': race_name_array, '階級': grade_array,
 #                          'コースの詳細': course_detail_array, 'コースの距離': course_dis_array, '天気': weather_array})
 # race_list.to_excel('./レースデータ.xlsx', header=False, index=False)
 
 # レースの詳細をエクセルファイルにエクスポート
 race_detail = pd.DataFrame(
-    {'着順':tyakujun_array,'馬番': umaban_array, '馬名': name_array, '斤量': add_weight_array, '年齢': age_array, '騎手': jockey_array, 'タイム': time_array, '体重': weight_array, '調教師': trainer_array, '着差': margin_array, 'コーナー通過': corner_array})
-race_detail.to_excel('./レース詳細.xlsx', header=False, index=False)
+    {'URL': url_array, '日付': date_array, 'レース名': race_name_array, '天気': weather_array, '発走時間(サブ)': hour_array_sub, '発走時間': hour_array, 'コースの距離': course_dis_array, '距離(サブ)': course_dis_array_sub, 'コースの詳細': course_detail_array, 'コースの状態': status_array, 'レースの各位': grade_array, '着順': tyakujun_array, '枠': waku_array, '馬番': umaban_array, '馬名': name_array, '斤量': add_weight_array, '年齢': age_array, '騎手': jockey_array, 'タイム': time_array, '推定上り': ftime_array, '体重': weight_array, '調教師': trainer_array, '着差': margin_array, 'コーナー通過': corner_array})
+
+
+race_detail.to_excel('./レース詳細.xlsx', index=False, sheet_name='all_data')
--- getData/getParents.py
+++ getData/getParents.py
@@ -1,3 +1,4 @@
+# coding UTF-8
 from bs4 import BeautifulSoup
 import os
 import re
@@ -14,12 +15,72 @@ year_url = []
 month_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 month_url = []
 race_url = []
+horse_url_array = []
 race_result = []
 horse_list = []
-father = []
-mother = []
-parents_whole = []
-parents_list = []
+
+GI_first = []
+GII_first = []
+GIII_first = []
+otherjusyo_first = []
+jusyo_sum_first = []
+special_first = []
+sum_first = []
+GI_second = []
+GII_second = []
+GIII_second = []
+otherjusyo_second = []
+jusyo_sum_second = []
+special_second = []
+sum_second = []
+GI_third = []
+GII_third = []
+GIII_third = []
+otherjusyo_third = []
+jusyo_sum_third = []
+special_third = []
+sum_third = []
+GI_fourth = []
+GII_fourth = []
+GIII_fourth = []
+otherjusyo_fourth = []
+jusyo_sum_fourth = []
+special_fourth = []
+sum_fourth = []
+GI_syussou = []
+GII_syussou = []
+GIII_syussou = []
+otherjusyo_syussou = []
+jusyo_sum_syussou = []
+special_syussou = []
+sum_syussou = []
+GI_vic = []
+GII_vic = []
+GIII_vic = []
+otherjusyo_vic = []
+jusyo_sum_vic = []
+special_vic = []
+sum_vic = []
+GI_rentai = []
+GII_rentai = []
+GIII_rentai = []
+otherjusyo_rentai = []
+jusyo_sum_rentai = []
+special_rentai = []
+sum_rentai = []
+first_temp = []
+second_temp = []
+third_temp = []
+fourth_temp = []
+syussou_temp = []
+vic_temp = []
+rentai_temp = []
+# father = []
+# mother = []
+# parents_whole = []
+# parents_list = []
+
+
 child = []
 
 for year in year_list:
@@ -43,10 +104,9 @@ for url in month_url:
     soup_race_result = BeautifulSoup(response.content, 'lxml')
     for href in soup_race_result.find_all("td", class_="wsLB"):
         # すべてのレースのURLを作成
-        # race_result.append(" https://keiba.yahoo.co.jp" +
-        #                    str(href.find("a").get("href")))
-        race_url_str = " https://keiba.yahoo.co.jp" + str(href.find("a").get("href"))
-            
+        race_url_str = " https://keiba.yahoo.co.jp" + \
+            str(href.find("a").get("href"))
+
         response = requests.get(url=race_url_str, headers=headers)
         soup = BeautifulSoup(response.content, 'lxml')
         for href in soup.find_all("td", class_="fntN"):
@@ -54,26 +114,204 @@ for url in month_url:
             horse_url = "https://keiba.yahoo.co.jp" + \
                 str(href.find("a").get("href"))
             print(horse_url)
+            horse_url_array.append(horse_url)
             response = requests.get(url=horse_url, headers=headers)
             soup = BeautifulSoup(response.content, 'lxml')
+            # データ削除されている馬があるのでそのエラーをパスする
             try:
-                for parents_list in soup.select("#dirUmaBlood > tr "):
-                    parents_whole.append(parents_list.select_one("td:nth-of-type(1)"))
-                father.append(parents_whole[0].text)
-                mother.append(parents_whole[4].text)
+            # 親の情報
+            # parents_whole = []
+            # for parents_list in soup.select("#dirUmaBlood > tr "):
+            #     parents_whole.append(
+            #         parents_list.select_one("td:nth-of-type(1)"))
+            # father.append(parents_whole[0].text)
+            # mother.append(parents_whole[4].text)
+
+
+                for race_result in soup.select(".dataLs.mgnBL > tr"):
+                    first_temp = []
+                    second_temp = []
+                    third_temp = []
+                    fourth_temp = []
+                    syussou_temp = []
+                    vic_temp = []
+                    rentai_temp = []
+                    first_temp.append(
+                        race_result.select_one("td:nth-of-type(1)"))
+                    second_temp.append(
+                        race_result.select_one("td:nth-of-type(2)"))
+                    third_temp.append(
+                        race_result.select_one("td:nth-of-type(3)"))
+                    fourth_temp.append(
+                        race_result.select_one("td:nth-of-type(4)"))
+                    syussou_temp.append(
+                        race_result.select_one("td:nth-of-type(5)"))
+                    vic_temp.append(
+                        race_result.select_one("td:nth-of-type(6)"))
+                    rentai_temp.append(
+                        race_result.select_one("td:nth-of-type(7)"))
+                GI_first.append(first_temp[0])
+                GII_first.append(first_temp[1])
+                GIII_first.append(first_temp[2])
+                otherjusyo_first.append(first_temp[3])
+                jusyo_sum_first.append(first_temp[4])
+                special_first.append(first_temp[5])
+                sum_first.append(first_temp[6])
+                GI_second.append(second_temp[0])
+                GII_second.append(second_temp[1])
+                GIII_second.append(second_temp[2])
+                otherjusyo_second.append(second_temp[3])
+                jusyo_sum_second.append(second_temp[4])
+                special_second.append(second_temp[5])
+                sum_second.append(second_temp[6])
+                GI_third.append(third_temp[0])
+                GII_third.append(third_temp[1])
+                GIII_third.append(third_temp[2])
+                otherjusyo_third.append(third_temp[3])
+                jusyo_sum_third.append(third_temp[4])
+                special_third.append(third_temp[5])
+                sum_third.append(third_temp[6])
+                GI_fourth.append(fourth_temp[0])
+                GII_fourth.append(fourth_temp[1])
+                GIII_fourth.append(fourth_temp[2])
+                otherjusyo_fourth.append(fourth_temp[3])
+                jusyo_sum_fourth.append(fourth_temp[4])
+                special_fourth.append(fourth_temp[5])
+                sum_fourth.append(fourth_temp[6])
+                GI_syussou.append(syussou_temp[0])
+                GII_syussou.append(syussou_temp[1])
+                GIII_syussou.append(syussou_temp[2])
+                otherjusyo_syussou.append(syussou_temp[3])
+                jusyo_sum_syussou.append(syussou_temp[4])
+                special_syussou.append(syussou_temp[5])
+                sum_syussou.append(syussou_temp[6])
+                GI_vic.append(vic_temp[0])
+                GII_vic.append(vic_temp[1])
+                GIII_vic.append(vic_temp[2])
+                otherjusyo_vic.append(vic_temp[3])
+                jusyo_sum_vic.append(vic_temp[4])
+                special_vic.append(vic_temp[5])
+                sum_vic.append(vic_temp[6])
+                GI_vic.append(rentai_temp[0])
+                GII_rentai.append(rentai_temp[1])
+                GIII_rentai.append(rentai_temp[2])
+                otherjusyo_rentai.append(rentai_temp[3])
+                jusyo_sum_rentai.append(rentai_temp[4])
+                special_rentai.append(rentai_temp[5])
+                sum_rentai.append(rentai_temp[6])
                 child.append(soup.find("h1", class_="fntB").get_text())
-                parents_whole = []
-                # print(father)
-                # print(mother)
-                counter -=1
-                print(counter)
             except:
                 pass
 
-print(len(child))
-print(len(mother))
-print(len(father))
+# print(len(mother))
+# print(len(father))
 
-parents_detail = pd.DataFrame(
-    {"仔馬": child, "父馬": father, "母馬": mother})
-parents_detail.to_excel('./血統.xlsx', header=False, index=False)
+print(len(child))
+print(len(GI_first))
+print(len(GII_first))
+print(len(GIII_first))
+print(len(otherjusyo_first))
+print(len(jusyo_sum_first))
+print(len(special_first))
+print(len(sum_first))
+print(len(GI_second))
+print(len(GII_second))
+print(len(GIII_second))
+print(len(otherjusyo_second))
+print(len(jusyo_sum_second))
+print(len(special_second))
+print(len(sum_second))
+print(len(GI_third))
+print(len(GII_third))
+print(len(GIII_third))
+print(len(otherjusyo_third))
+print(len(jusyo_sum_third))
+print(len(special_third))
+print(len(sum_third))
+print(len(GI_fourth))
+print(len(GII_fourth))
+print(len(GIII_fourth))
+print(len(otherjusyo_fourth))
+print(len(jusyo_sum_fourth))
+print(len(special_fourth))
+print(len(sum_fourth))
+print(len(GI_syussou))
+print(len(GII_syussou))
+print(len(GIII_syussou))
+print(len(otherjusyo_syussou))
+print(len(jusyo_sum_syussou))
+print(len(special_syussou))
+print(len(sum_syussou))
+print(len(GI_vic))
+print(len(GII_vic))
+print(len(GIII_vic))
+print(len(otherjusyo_vic))
+print(len(jusyo_sum_vic))
+print(len(special_vic))
+print(len(sum_vic))
+print(len(GI_rentai))
+print(len(GII_rentai))
+print(len(GIII_rentai))
+print(len(otherjusyo_rentai))
+print(len(jusyo_sum_rentai))
+print(len(special_rentai))
+print(len(sum_rentai))
+
+
+# parents_detail = pd.DataFrame(
+#     {"URL": horse_url_array, "仔馬": child, "父馬": father, "母馬": mother})
+race_num_data=pd.DataFrame(
+    {"URL": horse_url_array,
+     "馬名": child,
+     "G1一着": GI_first,
+     "G2一着": GII_first,
+     "G3一着": GIII_first,
+     "その他重賞一着": otherjusyo_first,
+     "重賞一着合計": jusyo_sum_first,
+     "特別一着": special_first,
+     "一着合計": sum_first,
+     "G1二着": GI_second,
+     "G2二着": GII_second,
+     "G3二着": GIII_second,
+     "その他重賞二着": otherjusyo_second,
+     "重賞二着合計": jusyo_sum_second,
+     "特別二着": special_second,
+     "合計二着": sum_second,
+     "G1三着": GI_third,
+     "G2三着": GII_third,
+     "G3三着": GIII_third,
+     "その他重賞三着": otherjusyo_third,
+     "重賞三着合計": jusyo_sum_third,
+     "特別三着": special_third,
+     "三着合計": sum_third,
+     "G1四着以下": GI_fourth,
+     "G2四着以下": GII_fourth,
+     "G3四着以下": GIII_fourth,
+     "その他四着以下": otherjusyo_fourth,
+     "その他重賞四着以下": jusyo_sum_fourth,
+     "四着以下": special_fourth,
+     "四着以下合計": sum_fourth,
+     "G1出走数": GI_syussou,
+     "G2出走数": GII_syussou,
+     "G3出走数": GIII_syussou,
+     "その他重賞出走数": otherjusyo_syussou,
+     "重賞出走数合計": jusyo_sum_syussou,
+     "特別出走数": special_syussou,
+     "出走数合計": sum_syussou,
+     "G1勝率": GI_vic,
+     "G2勝率": GII_vic,
+     "G3勝率": GIII_vic,
+     "その他重賞勝率": otherjusyo_vic,
+     "重賞勝率合計": jusyo_sum_vic,
+     "特別勝率": special_vic,
+     "勝率合計": sum_vic,
+     "G1連対数": GI_rentai,
+     "G2連対数": GII_rentai,
+     "G3連対数": GIII_rentai,
+     "その他重賞連対数": otherjusyo_rentai,
+     "重賞連対数合計": jusyo_sum_rentai,
+     "特別連対数": special_rentai,
+     "連対数合計": sum_rentai}
+     )
+# parents_detail.to_excel('./血統.xlsx', index=False, sheet_name="血統データ")
+race_num_data.to_excel('./血統.xlsx', index=False, sheet_name="出走データ")
