a93aa360c0db20421cf20c4ba35cb1e4288afc32
2190468 <2190468@ecccomp.com>
2021-06-22 12:45:59 +0900

    2021/06/22

--- expectRank/functions.py
+++ expectRank/functions.py
@@ -2,7 +2,7 @@ import openpyxl as openpyxl
 import numpy as np
 
 class Functions:
-    def toArray(self,min_row, min_col, max_row, max_col):
+    def toArray(self,min_rowswg, min_col, max_row, max_col):
         wb = openpyxl.load_workbook("./必要データ纏め.xlsx")
         sheet = wb["マスタデータレース一覧"]
         tenkou = []
